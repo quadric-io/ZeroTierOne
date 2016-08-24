1839cc9f20643c784d0f087405a4b7afbba27dd5
Nigel Drego <nigel@quadric.io>
2016-08-24 22:06:15 +0000

    Fix to compile on Ubuntu 14.04 LTS (arm64)

--- make-linux.mk
+++ make-linux.mk
@@ -22,10 +22,10 @@
 # Automagically pick clang or gcc, with preference for clang
 # This is only done if we have not overridden these with an environment or CLI variable
 ifeq ($(origin CC),default)
-	CC=$(shell if [ -e /usr/bin/clang ]; then echo clang; else echo gcc; fi)
+	CC=$(shell if [ -e /usr/bin/clang ]; then echo clang; else echo gcc-4.9; fi)
 endif
 ifeq ($(origin CXX),default)
-	CXX=$(shell if [ -e /usr/bin/clang++ ]; then echo clang++; else echo g++; fi)
+	CXX=$(shell if [ -e /usr/bin/clang++ ]; then echo clang++; else echo g++-4.9; fi)
 endif
 
 #UNAME_M=$(shell $(CC) -dumpmachine | cut -d '-' -f 1)
